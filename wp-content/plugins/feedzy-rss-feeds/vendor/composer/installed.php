09127d3de116f36a50e4bea226ab193b1adb4542
root <namydragon@qq.com>
2021-12-08 10:04:25 +0000

    Wed Dec  8 10:04:25 UTC 2021

---
+++ wp-content/plugins/feedzy-rss-feeds/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v3.8.0',
+        'version' => '3.8.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '59cce95cac69d1a6d94e3e60cf77c7cbc4814f2b',
+        'name' => 'codeinwp/feedzy-rss-feeds',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/feedzy-rss-feeds' => array(
+            'pretty_version' => 'v3.8.0',
+            'version' => '3.8.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '59cce95cac69d1a6d94e3e60cf77c7cbc4814f2b',
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'aeef3f159c1b35451d87672b6984ccde36c0d21d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
