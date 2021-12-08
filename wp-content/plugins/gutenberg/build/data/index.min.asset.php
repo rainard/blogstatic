2f3f54625b6baf7956c0c8a1efc73e6dbbff25e9
root <namydragon@qq.com>
2021-12-08 06:15:16 +0000

    Wed Dec  8 06:15:16 UTC 2021

---
+++ wp-content/plugins/gutenberg/build/data/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-compose', 'wp-deprecated', 'wp-element', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-priority-queue', 'wp-redux-routine'), 'version' => '45683cf15ae0a980aca8b518a02d85fc');
